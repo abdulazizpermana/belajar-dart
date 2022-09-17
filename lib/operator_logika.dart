c1787aba25ddafbb5a818109ef99ca0eabe46c9d
abdul <abdulazizpermana106@gmail.com>
2022-09-17 14:52:07 +0700

    second commit

--- lib/operator_logika.dart
+++ lib/operator_logika.dart
@@ -1,3 +1,19 @@
 void main(){
+  var nilaiAkhir = 80;
+  var nilaiAbsen = 60;
+
+  var apakahNilaiAkhirBagus = nilaiAkhir >= 75;
+  var apakahNilaiAbsenBagus = nilaiAbsen >= 75;
+
+  print(apakahNilaiAkhirBagus);
+  print(apakahNilaiAbsenBagus);
+
+
+  // var lulus = apakahNilaiAbsenBagus && apakahNilaiAkhirBagus;
+  var lulus = apakahNilaiAbsenBagus || apakahNilaiAkhirBagus;
+  print(lulus);
+
+  print(!false);
+  print(!true);
 
 }
